fd8ca76f58dc77a8230cc53149d14e3c4f736f19
zdazx <zhangduo0227@163.com>
2020-07-28 21:13:29 +0800

    add my application

---
+++ app/src/main/java/com/thoughtworks/loginexample/MyApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.loginexample;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
